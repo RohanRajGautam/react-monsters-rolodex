1b538a391f24cdec091dd6a1cbd40051b075db95
rohan <rohanrajgautam@gmail.com>
2020-07-14 22:44:30 +0545

    Updates

---
+++ precache-manifest.871904d6ff448c2c2fc3721cb8f6a29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3450d698aeb8dc261c8e42f270d7dfb1",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "82e4ab04ff5dd086531a",
+    "url": "/react-monsters-rolodex/static/css/main.cb5cf897.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e4ab04ff5dd086531a",
+    "url": "/react-monsters-rolodex/static/js/main.90082335.chunk.js"
+  },
+  {
+    "revision": "84cac6f49c2db027c893",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.782a98e2.js"
+  }
+]);
